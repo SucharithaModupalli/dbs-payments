e79b4865746bd1b3dad5f8e9cf4947c25eb8a242
Sucharitha Modupalli <48296189+SucharithaModupalli@users.noreply.github.com>
2021-10-08 08:53:47 +0530

    Update CustomerRepo.java

--- project1/src/main/java/com/dbs/dao/CustomerRepo.java
+++ project1/src/main/java/com/dbs/dao/CustomerRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.dbs.beans.customer;
 
 @Repository
-public interface CustomerRepo extends JpaRepository< customer, String> {
+public interface CustomerRepo extends JpaRepository< customer, Integer> {
 
 }
