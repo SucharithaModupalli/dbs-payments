e05ba9040a205aa1b7205692100e0857c312fe90
Sucharitha Modupalli <48296189+SucharithaModupalli@users.noreply.github.com>
2021-10-11 13:51:45 +0530

    Update CustomerController.java

--- project1/src/main/java/com/dbs/controller/CustomerController.java
+++ project1/src/main/java/com/dbs/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.dbs.controller;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
